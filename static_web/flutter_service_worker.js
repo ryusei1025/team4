428eddae5f2efa9e16c888a0e54c883413f0cef6
Spencer <241108@st.yoshida-g.ac.jp>
2026-02-18 10:41:27 +0900

    Update gemini api key

--- static_web/flutter_service_worker.js
+++ static_web/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "84a4a6e159e704935a486c795d6aae55
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4e153a55d23fbe09c66d227c4b0615b9",
+"flutter_bootstrap.js": "3a7bfd0413c3bf654efbc0b82e165019",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
