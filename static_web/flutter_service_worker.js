92ade1ff9b85158091262aef3e6dd24fd769173a
Spencer <241108@st.yoshida-g.ac.jp>
2026-02-18 12:03:01 +0900

    Fix backend files and ignore list

--- static_web/flutter_service_worker.js
+++ static_web/flutter_service_worker.js
@@ -43,14 +43,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "84a4a6e159e704935a486c795d6aae55
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "50aa3b6b47e071fad5d31acdb8f9e2e1",
+"flutter_bootstrap.js": "24c904a65d4e836e457225e4c2e6a5f6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "900f510116f6782449d53979ae1de285",
 "/": "900f510116f6782449d53979ae1de285",
-"main.dart.js": "93b9eb4c54c894b18f097d973a3e2a22",
+"main.dart.js": "2f8ec8fb4927e6f55040f0afd5e42e27",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
